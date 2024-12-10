2cb281e55a14a05b3f7c1b0a63accdf8caff951f
Napapa Khumphuean <napapa.khumphuean@gmail.com>
2024-12-10 20:48:50 +0700

    Update ATMService.java

--- HW2/ATMService.java
+++ HW2/ATMService.java
@@ -1,7 +1,7 @@
 public interface ATMService {
-    void checkBalance(String accountID);
-    void withdraw(String accountID);
-    void deposit(String accountID);
-    void transfer(Account_Bank sourceAccount);
-    void exit();
+    void checkable(String accountID);
+    void withdrawalable(String accountID);
+    void depositeable(String accountID);
+    void tranferable(Account_Bank sourceAccount);
+    void exitable();
 }
